14b089d954727c545d5f7816f35dfd485fd1c1ec
Zachary Fowler <zachary.fowler1@stu.ocsb.ca>
2025-10-03 23:27:14 +0000

    Created code that prints name

---
+++ main.go
@@ -0,0 +1,17 @@
+/*
+ * @author: Zachary Fowler
+ * @version:1.0.0
+ * @date : 2025-10-3
+ * @fileoverview: Displaying name
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	//print out name
+	fmt.Println("Zachary Fowler")
+
+	fmt.Println("\nDone.")
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author: Zachary Fowler
+ * @version:1.0.0
+ * @date : 2025-10-3
+ * @fileoverview: Displaying name
+ */
+
+//Print name
+console.log("Zachary Fowler");
+
+console.log("\ndone");
